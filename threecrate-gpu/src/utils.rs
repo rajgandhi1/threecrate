e9a024abfa34cb6ac141cf1b7c3adb082141f8e6
rajgandhi1 <rajgandhi9952@gmail.com>
2025-08-09 12:23:23 +0530

    implement shader module

--- threecrate-gpu/src/utils.rs
+++ threecrate-gpu/src/utils.rs
@@ -4,6 +4,10 @@ use threecrate_core::Result;
 
 /// Create a compute shader from WGSL source
 pub fn create_compute_shader(_device: &wgpu::Device, _source: &str) -> Result<wgpu::ShaderModule> {
-    // TODO: Implement shader creation
-    todo!("Shader creation not yet implemented")
+    // Note: Shader module creation is infallible; validation happens at pipeline creation time
+    let module = _device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: Some("ThreeCrate Compute Shader"),
+        source: wgpu::ShaderSource::Wgsl(_source.into()),
+    });
+    Ok(module)
 } 
